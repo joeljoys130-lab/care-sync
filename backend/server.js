ee11b7fd7f3ac20c953af867ae06039b289a8ba5
joel <joel.joy.s.130@kalvium.community>
2026-04-17 16:23:11 +0530

    add project files

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const { protect } = require("./middleware/authMiddleware");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/test", protect, (req, res) => {
+  res.json({
+    message: "Protected route working",
+    user: req.user,
+  });
+});
+
+app.use("/api/patient", require("./routes/patientRoutes"));
+app.use("/api/doctor", require("./routes/doctorRoutes"));
+app.use("/api/admin", require("./routes/adminRoutes"));
+app.use("/api/payment", require("./routes/paymentRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
